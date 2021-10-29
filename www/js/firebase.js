66299643e401e1fd3249fb84976912c43e464149
ViniciusAltieri <vikeoch@hotmail.com>
2021-10-29 20:32:22 -0300

    Initial commit

---
+++ www/js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCqrTvzRIJK3hYz05yh3bszbPgXU05sYU8",
+    authDomain: "appagendamento-46e19.firebaseapp.com",
+    projectId: "appagendamento-46e19",
+    storageBucket: "appagendamento-46e19.appspot.com",
+    messagingSenderId: "94802482144",
+    appId: "1:94802482144:web:ebcfbdb42685e45ea59185",
+    measurementId: "G-RNB1QTDJ26"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
